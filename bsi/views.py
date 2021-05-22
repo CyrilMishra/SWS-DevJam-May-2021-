e57743da061984148bcd3dbcbbf5ec7560a067c4
Cyril Mishra <76900078+CyrilMishra@users.noreply.github.com>
2021-05-22 08:33:41 +0530

    bsi(edit remove complete)2

--- bsi/views.py
+++ bsi/views.py
@@ -30,17 +30,17 @@ def bsihome(request,):
 def sell(request):
 	# print(request.POST)
 
-	# student_id = Signup.objects.get(student_id=request.session['user_id_session_login'])
+	student_id = Signup.objects.get(student_id=request.session['user_id_session_login'])
 	form = SellForm()
 	if request.method == 'POST':
 		form = SellForm(request.POST)
-		# c_id = request.POST.get('student')
-		# c_stu_id = Signup.objects.get(id=c_id)
-		# print(c_id)
-		# print(c_stu_id)
-		# print(student_id)
-		# if c_stu_id != student_id :
-		# 	return HttpResponse("<strong>Please Select You User Id to Post Add.</strong><br><a href='sell'><button>Click Hare To Try again</a>")
+		c_id = request.POST.get('student')
+		c_stu_id = Signup.objects.get(id=c_id)
+		print(c_id)
+		print(c_stu_id)
+		print(student_id)
+		if c_stu_id != student_id :
+			return HttpResponse("<strong>Please Select You User Id to Post Add.</strong><br><a href='sell'><button>Click Hare To Try again</a>")
 		if form.is_valid():
 			form.save()
 			return redirect('sellpost')
